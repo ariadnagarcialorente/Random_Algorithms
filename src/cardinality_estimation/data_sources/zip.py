b606ad800a0149efa0a84700bf2d7476093583f4
Ariadna <ariadnag065@gmail.com>
2026-01-02 16:45:32 +0100

    hi

--- src/cardinality_estimation/data_sources/zip.py
+++ src/cardinality_estimation/data_sources/zip.py
@@ -7,6 +7,8 @@ class ZipfianGenerator:
         self.N_length = N_length
         self.alpha = alpha
 
+        total = 0
+
         for i in range(1, n_distinct + 1):
             total += 1.0 / (i ** -alpha)
         
